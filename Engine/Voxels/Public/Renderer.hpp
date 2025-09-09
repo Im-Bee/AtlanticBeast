ef95f0c4c42d13ad99876e0c76201abccbb6b50a
Im-Bee <matt.ib.g.12@gmail.com>
2025-09-09 18:57:58 +0200

    Cmake changes, small changes

--- Engine/Voxels/Public/Renderer.hpp
+++ Engine/Voxels/Public/Renderer.hpp
@@ -1,6 +1,7 @@
 #ifndef AB_RENDERER_H
 #define AB_RENDERER_H
 
-
+#include "Instance.hpp"
+#include "Hardware.hpp"
 
 #endif // !AB_RENDERER_H
--- Engine/Voxels/Public/Voxels.h
+++ Engine/Voxels/Public/Voxels.h
@@ -1,6 +1,10 @@
 #ifndef AB_VOXELS_H
 #define AB_VOXELS_H
 
+#include <vulkan/vulkan.hpp>
+#include <vulkan/vulkan_core.h>
+
+#include "Core.h"
 #include "Renderer.hpp"
 
 #endif // !AB_VOXELS_H
