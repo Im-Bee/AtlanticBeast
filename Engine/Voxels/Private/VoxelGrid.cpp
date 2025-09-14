7ac309744c359ac142a9be567337ccd53968854c
Im-Bee <ImBee@EGGACROPOLIS>
2025-09-14 23:47:48 +0200

    TWO CUBES!!!

--- Engine/Voxels/Private/VoxelGrid.cpp
+++ Engine/Voxels/Private/VoxelGrid.cpp
@@ -21,8 +21,9 @@ vector<Voxel> VoxelGrid::GenerateGrid()
     }
 
     size_t uIndex;
-    size_t uCubeStart   = 22;
+    size_t uCubeStart   = 33;
     size_t uCubeEnd     = 44;
+	voxelGrid[500].Type = 1;
     for (size_t z = uCubeStart; z < uCubeEnd; ++z) {
         for (size_t y = uCubeStart; y < uCubeEnd; ++y) {
             for (size_t x = uCubeStart; x < uCubeEnd; ++x) {
