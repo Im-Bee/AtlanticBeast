1891e6194490e31fe652d62e4febe741966e9425
Im-Bee <im.bee.vibe@gmail.com>
2025-10-10 00:01:54 +0200

    ...

--- Engine/Voxels/Public/Vulkan/WrapperAdapter.hpp
+++ Engine/Voxels/Public/Vulkan/WrapperAdapter.hpp
@@ -10,24 +10,12 @@ class Adapter
 {
 public:
 
-    Adapter()
-        : m_uQueueFamily(0)
-        , m_Device(VK_NULL_HANDLE)
-        , m_Queue(VK_NULL_HANDLE)
-    { }
-
-    Adapter(uint32_t uQueueIndex,
-                   VkDevice device,
-                   VkQueue queue)
-        : m_uQueueFamily(uQueueIndex)
-        , m_Device(device)
-        , m_Queue(queue)
-    { }
+    Adapter() = delete;
 
     Adapter(VkPhysicalDevice gpu, 
-                   uint32_t uFlags,
-                   const std::vector<const char*>& vExtensions,
-                   const void* pFeatures)
+            uint32_t uFlags,
+            const std::vector<const char*>& vExtensions,
+            const void* pFeatures)
         : m_uQueueFamily(ChooseQueueFamily(gpu, uFlags))
         , m_Device(CreateDevice(gpu, vExtensions, pFeatures, m_uQueueFamily))
         , m_Queue(CreateQueue(m_Device, m_uQueueFamily))
